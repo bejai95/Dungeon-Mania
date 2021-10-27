a1fd67143c4e815fad28bedea0d7ae16477b3729
Tommy Sayvong <t.sayvong@student.unsw.edu.au>
2021-10-27 20:32:13 +1100

    Movement interface and method stubs for movement implementations

---
+++ src/main/java/dungeonmania/RandomMovement.java
@@ -0,0 +1,12 @@
+package dungeonmania;
+
+public class RandomMovement implements Movement {
+    
+    /**
+     * Move object by 1 in a random cardinal direction
+     */
+    public void move() {
+
+    }
+
+}
