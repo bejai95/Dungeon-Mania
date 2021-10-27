5e3092bf8009c7cdfec6020215798e12d6f2184a
jeremy2805 <jeremy@orbitcoms.com>
2021-10-27 13:42:11 +1100

    added item test stub

---
+++ src/test/java/dungeonmania/ItemTest.java
@@ -0,0 +1,13 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+/**
+ * Tests all the item subclasses and there functionality
+ */
+public class ItemTest {
+    
+}
