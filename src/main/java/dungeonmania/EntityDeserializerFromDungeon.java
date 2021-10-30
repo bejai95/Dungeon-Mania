85e33204706ec0f3281a79e17934ea3853ffd32d
Bejai Cobbing <z5313120@ad.unsw.edu.au>
2021-10-30 17:19:36 +1100

    Backing up changes so I can get stuff from master

--- src/main/java/dungeonmania/EntityDeserializerFromDungeon.java
+++ src/main/java/dungeonmania/EntityDeserializerFromDungeon.java
@@ -1,18 +1,14 @@
 package dungeonmania;
-
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.google.gson.JsonArray;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
-
 import dungeonmania.util.Position;
-
 public class EntityDeserializerFromDungeon  implements JsonDeserializer<Entity> {
     
     @Override
@@ -20,17 +16,54 @@ public class EntityDeserializerFromDungeon  implements JsonDeserializer<Entity> 
             JsonDeserializationContext context) throws JsonParseException {
         
         JsonObject jsonObject = json.getAsJsonObject();
-
         // Generate an Id for the new entity
         int newEntityId = Entity.getNumEntityIds();
+        int x = jsonObject.get("x").getAsInt();
+        int y = jsonObject.get("y").getAsInt();
             
         switch(jsonObject.get("type").getAsString()) {
-            case "wall":
-                Position positionWall = new Position(jsonObject.get("x").getAsInt(), jsonObject.get("y").getAsInt(), 3);
-                return new Wall(newEntityId, "wall", positionWall);
             case "player":
-                Position positionPlayer = new Position(jsonObject.get("x").getAsInt(), jsonObject.get("y").getAsInt(), 2);
+                Position positionPlayer = new Position(x, y, 2);
                 return new Character(newEntityId, "player", positionPlayer);
+            case "wall":
+                Position positionWall = new Position(x, y, 3);
+                return new Wall(newEntityId, "wall", positionWall);
+            case "exit":
+                Position positionExit = new Position(x, y, 3);
+                return new Exit(newEntityId, "exit", positionExit);
+            case "boulder":
+                Position positionBoulder = new Position(x, y, 3);
+                return new Boulder(newEntityId, "boulder", positionBoulder);
+            case "switch":
+                Position positionSwitch = new Position(x, y, 3);
+                return new FloorSwitch(newEntityId, "switch", positionSwitch);
+            case "door":
+                Position positionDoor = new Position(x, y, 3);
+                // TODO logic in game that gets the matching key for the door
+                return new Door(newEntityId, "door", positionDoor, null);
+            case "portal":
+                Position positionPortal = new Position(x, y, 3);
+                // TODO logic in game that gets the matching portal
+                return new Portal(newEntityId, "portal", positionPortal, null);
+            case "zombie_toast_spawner":
+                Position positionSpawner = new Position(x, y, 3);
+                return new ZombieToastSpawner(newEntityId, "zombie_toast_spawner", positionSpawner);
+            case "mercenary":
+                Position positionMercenary = new Position(x, y, 3);
+                // TODO get other logic from 
+                return new Mercenary(newEntityId, "mercenary", positionMercenary);
+            
+            
+                case "wall":
+                Position positionWall = new Position(x, y, 3);
+                return new Wall(newEntityId, "wall", positionWall);
+            
+            
+
+            
+            
+            
+            
             }
         
         return null;
