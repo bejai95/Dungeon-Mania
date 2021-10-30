63b2ae39cf82c01ecd5d4c121e653450889bc822
jeremy2805 <jeremy@orbitcoms.com>
2021-10-30 14:40:44 +1100

    got rid of merc tests

--- src/test/java/dungeonmania/CharacterTest.java
+++ src/test/java/dungeonmania/CharacterTest.java
@@ -129,9 +129,4 @@ public class CharacterTest {
         //bow should be used up
         assertThrows(InvalidActionException.class, ()-> {character.use(bow);});
     }
-    @Test
-    public void testMercs() {
-        
-    }
-
 }
