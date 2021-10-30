10dbd724533fbbab3459ae26f09bfc1c2f6a0c4d
Brendan Efrossynis <b.efrossynis@student.unsw.edu.au>
2021-10-31 10:13:26 +1100

    Transfer the ID instead of having two sepperate id

--- src/main/java/dungeonmania/UnpickedUpItem.java
+++ src/main/java/dungeonmania/UnpickedUpItem.java
@@ -7,29 +7,28 @@ import java.lang.reflect.*;
 public class UnpickedUpItem extends StaticEntity {
     //-----Data-----
     private String itemClass;
-    private int itemid;
 
     //-----Constructors-----
-    /*Be careful not to get inputs mixed up:
+    /*
     id = The id of the unpickedup Static Entity (E.g. 2)
-    itemid = The id of the item (E.g. 5)
     itemClass = "HealthPotion"
+    Note the ID gets transferred to the item on pickup
     */
-    public UnpickedUpItem(int id, String type, Position position, String itemClass, int itemid) {
+    public UnpickedUpItem(int id, String type, Position position, String itemClass) {
         super(id, "unpickedup_item", position);
         this.itemClass = itemClass;
-        this.itemid = itemid;
     }
 
     //-----Methods-----
     //Method for picking up an item
     //Note: I am temporarily Using 0 for ID until ID system implemented
+    //Note the ID gets transferred to the item on pickup
     public Item pickupItem () throws ClassNotFoundException, NoSuchMethodException, InstantiationException,
     IllegalAccessError, IllegalAccessException, InvocationTargetException {
         //Create the new item
         Class classType = Class.forName(itemClass);
         Constructor construct = classType.getConstructor(int.class);
-        Item newItem = (Item)construct.newInstance(itemid);
+        Item newItem = (Item)construct.newInstance(id);
 
         //Remove this object from the static entities list
         super.removeStaticEntity();
