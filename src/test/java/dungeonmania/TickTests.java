82ea336c8b79bc50e0dba9ecaa6d57fb2f7e5e18
Raph-DG <raphaeldouglasgiles@gmail.com>
2021-10-30 17:45:36 +1100

    Changed name of Character tests to tick tests

--- src/test/java/dungeonmania/TickTests.java
+++ src/test/java/dungeonmania/TickTests.java
@@ -15,7 +15,7 @@ import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.response.models.EntityResponse;
 import dungeonmania.util.Direction;
 
-public class CharacterTest {
+public class TickTests {
     
     private EntityResponse getPlayer(List<EntityResponse> entities){
         for(EntityResponse entity : entities){
