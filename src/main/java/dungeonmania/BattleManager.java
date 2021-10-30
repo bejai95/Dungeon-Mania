ef589fcebbf3157dfdd63524293d74c105ca9adb
Raph-DG <raphaeldouglasgiles@gmail.com>
2021-10-31 10:01:01 +1100

    Added a tentative implementation ogf battles

--- src/main/java/dungeonmania/BattleManager.java
+++ src/main/java/dungeonmania/BattleManager.java
@@ -1,6 +1,7 @@
 package dungeonmania;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -21,9 +22,29 @@ public class BattleManager {
         goodie.setHealth(((goodie.getHealth() - (baddie.getHealth() * baddie.getDamage()))/10)*goodie.getDefenceMultiplier());
         baddie.setHealth(((baddie.getHealth() - (goodie.getHealth() * goodie.getDamage()))/5)*baddie.getDefenceMultiplier());
         
-        return ret.stream().filter(x -> x.getHealth() > 0).collect(Collectors.toList());
+        return ret.stream().filter(x -> x.getHealth() <= 0).collect(Collectors.toList());
     }
     
+    private List<Mercenary> getAllies(List<Mercenary> mercs){
+        return null; //TODO
+    }
+
+    private List<Mercenary> getEnemies(List<Mercenary> mercs){
+        return null; //TODO
+    }
+
+
+    private List<Mercenary> getEnemiesInRange(List<Mercenary> mercs, Character player){
+        return null; //TODO
+    }
+
+    private List<Mercenary> getAlliesInRange(List<Mercenary> mercs, Battleable baddie){
+        return mercs.stream().filter(x -> !x.isHostile && x.entityInRadius(baddie)).collect(Collectors.toList()); 
+    }
+
+    private List<Mercenary> getMercsInRange(List<Mercenary> mercenaries, Character player){
+        return mercenaries.stream().filter(x -> x.entityInRadius(player)).collect(Collectors.toList());
+    }
     /**
      * Starts a battle instance for the player and a moving entity
      * which ends when one of the entites dies.
@@ -34,23 +55,26 @@ public class BattleManager {
      * @param baddie
      * @return
      */
-    public static Battleable battle(Character player, Battleable baddie){        
-        List<Battleable> allies = new ArrayList<>();
-        allies.add(player);
-        allies.addAll(player.getAllies());
-        
-        for(Battleable ally : allies){
-
-        }
-        while(player.getHealth() >= 0 && baddie.getHealth() >= 0){
-            player.setHealth((player.getHealth() - (baddie.getHealth() * baddie.getDamage()))/10);
-            baddie.setHealth((baddie.getHealth() - (player.getHealth() * player.getDamage()))/5);
+    public void battle(Character player, MovingEntity baddie, List<Mercenary> mercenaries){        
+        List<Battleable> alive = new ArrayList<>();
+        alive.add(player);
+        alive.addAll();
+        List<Mercenary> allies = getAlliesInRange(mercenaries, player);
+        List<Mercenary> mercsInRange = getMercsInRange(mercenaries, player);
+        for(Mercenary merc : mercsInRange){
+            merc.doubleSpeed();
+            //Remember to undo this in the tick function after everyone moves
         }
 
-        if(player.getHealth() < 0){
-            return player;
+        while(player.getHealth() > 0 && baddie.getHealth() > 0){
+            alive.removeAll(battleInstance(player, baddie));
+            for(Mercenary merc : allies){
+                if(!alive.contains(baddie)){
+                    break;
+                }
+                alive.removeAll(battleInstance(merc, baddie));
+            }
         }
-        return baddie;
     }
 
     
