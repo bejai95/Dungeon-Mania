25f787e45cab4a44fe44c198222ec527a7761c16
Raph-DG <raphaeldouglasgiles@gmail.com>
2021-11-01 10:05:53 +1100

    Changed AND and OR to account for >2 subgoals

--- src/main/java/dungeonmania/AndGoal.java
+++ src/main/java/dungeonmania/AndGoal.java
@@ -1,6 +1,7 @@
 package dungeonmania;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class AndGoal implements Goal{
     private List<Goal> subgoals;
@@ -8,20 +9,30 @@ public class AndGoal implements Goal{
     public AndGoal(List<Goal> subgoals){
         this.subgoals = subgoals;
     }
+    
+    private String andifiy(List<String> gs){
+        if(gs.size() == 0){
+            return "";
+        }
+
+        if(gs.size() == 1){
+            return gs.get(0);
+        }
+        
+        String ret = "(";
+        for(String g : gs){
+            if(gs.get(gs.size() - 1).equals(g)){
+                ret = ret + g + ")";
+            }else{
+                ret = ret + g + " AND ";
+            }
+        }
+        return ret;
+    }
     @Override
     public String getGoalsLeft(List<Entity> entities) {
-        Goal subgoal1 = subgoals.get(0);
-        Goal subgoal2 = subgoals.get(1);
-        String conj1 = subgoal1.getGoalsLeft(entities);
-        if(conj1.equals("")){
-            return subgoal2.getGoalsLeft(entities);
-        }
-        String conj2 = subgoal2.getGoalsLeft(entities);
-        if(conj2.equals("")){
-            return conj1;
-        }else{
-            return "(" + conj1 + " AND " + conj2 + ")"; 
-        }
+        List<String> subgoalStrings = subgoals.stream().map(x -> x.getGoalsLeft(entities)).filter(x -> !x.equals("")).collect(Collectors.toList());
+        return andifiy(subgoalStrings);
     }
 
     
