c9011d7654a35908922a0adac1313ed2b54d4b58
Brendan Efrossynis <b.efrossynis@student.unsw.edu.au>
2021-11-09 02:10:14 +1100

    potal and static entity fixup

--- src/main/java/dungeonmania/Portal.java
+++ src/main/java/dungeonmania/Portal.java
@@ -11,27 +11,29 @@ public class Portal extends StaticEntity {
     //-----Data-----
     private String potalColour;
 
-    //Contains a list of all portals on the map
-    private static List<Portal> portalList = new ArrayList<Portal>();
 
     //-----Constructors-----
     public Portal(int id, String type, Position position, String potalColour) {
         super(id, "potal", position);
         this.potalColour = potalColour;
-        portalList.add(this);
     }
 
     //-----Methods-----
-    //Gets the location for where the caracter should teleport to 
-    public Position getTeleportLocation(){
-        for (Portal portalItem : portalList) {
-            if ((portalItem != this) && (portalItem.getPotalColour() == this.potalColour)) {
-                return portalItem.getPosition();
+    /*When given a list of static entities this finds the coresponding portal and
+    finds the location for where the character should teleport to */
+    public Position getTeleportLocation(List<StaticEntity> staticEntitiesList){
+        for (StaticEntity staticEntityItem : staticEntitiesList) {
+            if (staticEntityItem instanceof Portal) {
+                Portal potentialPortal = (Portal)staticEntityItem;
+                if ((potentialPortal != this) && (potentialPortal.getPotalColour() == this.potalColour)) {
+                    return potentialPortal.getPosition();
+                }
             }
         }
         return getPosition();
     }
 
+    //-----Getters and Setters-----
     public String getPotalColour() {
         return potalColour;
     }
