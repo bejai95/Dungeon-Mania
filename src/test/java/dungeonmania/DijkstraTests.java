c59737158b9de9d685f610e0797221f209a4f759
Raph-DG <raphaeldouglasgiles@gmail.com>
2021-11-13 22:38:40 +1100

    Need to change branch

---
+++ src/test/java/dungeonmania/DijkstraTests.java
@@ -0,0 +1,48 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.exceptions.InvalidActionException;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.util.Direction;
+
+public class DijkstraTests {
+    @Test
+    public void testNoObstructionStraight() {
+        
+    }
+
+    @Test
+    public void testNoObstructionBent() {
+        
+    }
+
+    @Test
+    public void testObstructionStraight() {
+        
+    }
+
+    @Test
+    public void testObstructionBent() {
+        
+    }
+
+    @Test
+    public void testSwampStraight() {
+        
+    }
+
+    
+
+
+
+}
